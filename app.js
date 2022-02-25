45c4eaa786dd15cd72f0f693b7851093a49e7374
kristkas <kristin.kasuk@gmail.com>
2022-02-25 14:33:15 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', cityWeather)
 
+
 function weatherDataFetch( city ) {
     var key = '77f97b598769bf8861004c74e30ebbba';
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}` )
@@ -8,6 +9,7 @@ function weatherDataFetch( city ) {
         }) // Convert data to json
         .then( function(data) {
             console.log(data);
+            drawWeather(data);
         })
         .catch(function() {
             // catch any errors
@@ -17,3 +19,12 @@ function weatherDataFetch( city ) {
 function cityWeather(e){
     weatherDataFetch('Tallinn');
 }
+
+function drawWeather( data ) {
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg; ';
+    document.querySelector('#location').innerHTML = data.name;
+    }
